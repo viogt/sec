79e833b253f596284b110f780462e4e575dd3f6b
viogt <12400335+viogt@users.noreply.github.com>
2024-09-22 18:17:09 +0200

    Add files via upload

--- files/raider/script.js
+++ files/raider/script.js
@@ -15,3 +15,28 @@ function showContent(obj) {
     obj.innerText = 'Hide the table of contents';
   }
 }
+
+function handleFx() {
+  const el = event.target.id;
+  if(el == 'inceput') document.documentElement.scrollTop = 0;
+  else if(el == 'sfarsit') document.documentElement.scrollTop = document.documentElement.scrollHeight;
+  else window.location.href = "https://mda-today.blogspot.com/2018/03/brief-note-on-raider-attacks.html";
+}
+
+function createPanel() {
+  const d = document.createElement('div'),
+      a1 = document.createElement('a'),
+      a2 = document.createElement('a'),
+      a3 = document.createElement('a');
+  d.className = "fx_div";
+  a1.id = "inceput"; a3.id = "sfarsit";
+  a1.className = "fx"; a1.innerText = '⇱';
+  a2.className = "fx"; a2.innerText = '☰';
+  a3.className = "fx"; a3.innerText = '⇲';
+  a1.addEventListener("click", handleFx); d.appendChild(a1);
+  a2.addEventListener("click", handleFx); d.appendChild(a2);
+  a3.addEventListener("click", handleFx); d.appendChild(a3);
+  document.body.appendChild(d);
+}
+
+setTimeout(createPanel, 500);
